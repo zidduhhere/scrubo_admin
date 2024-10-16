5198ab0752ac3f1b57c7af22f544175e6e64ce1f
Abi Alif <abi.alif@acetvm.com>
2024-10-17 00:13:36 +0530

    Python Removed

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import os
 import pathlib
+from venv import create
 
 
 def create_paths(path_list):
-    """
-    Creates folders and files specified in a list of paths.
-
-    Args:
-        path_list: A list of strings, where each string is a full path
-                   to a file or folder to be created.
-                   e.g., ["/tmp/myfolder", "/tmp/myfile.txt", "/home/user/anotherfolder/file2.log"]
-
-    """
     for path_str in path_list:
         path = pathlib.Path(path_str)  # Use pathlib for better path handling
 
@@ -40,31 +32,58 @@ folders_and_files = [
     "lib/models/user_model.dart",
     "lib/models/service_model.dart",
     "lib/models/booking_model.dart",
-    "lib/models/review_model.dart",
+    "lib/models/customer_model.dart",
+    "lib/models/report_model.dart",
+    "lib/models/notification_model.dart",
     "lib/views/login/login_view.dart",
     "lib/views/login/signup_view.dart",
-    "lib/views/home/home_view.dart",
-    "lib/views/service_details/service_details_view.dart",
-    "lib/views/service_scheduling/service_scheduling_view.dart",
-    "lib/views/payment/payment_view.dart",
-    "lib/views/booking_history/booking_history_view.dart",
-    "lib/views/profile_management/profile_management_view.dart",
-    "lib/views/ratings_reviews/ratings_reviews_view.dart",
+    "lib/views/dashboard/dashboard_view.dart",
+    "lib/views/service_management/service_management_view.dart",
+    "lib/views/booking_management/booking_management_view.dart",
+    "lib/views/customer_management/customer_management_view.dart",
+    "lib/views/reports_analytics/reports_analytics_view.dart",
+    "lib/views/notifications/notifications_view.dart",
     "lib/viewmodels/login/login_viewmodel.dart",
     "lib/viewmodels/login/signup_viewmodel.dart",
-    "lib/viewmodels/home/home_viewmodel.dart",
-    "lib/viewmodels/service_details/service_details_viewmodel.dart",
-    "lib/viewmodels/service_scheduling/service_scheduling_viewmodel.dart",
-    "lib/viewmodels/payment/payment_viewmodel.dart",
-    "lib/viewmodels/booking_history/booking_history_viewmodel.dart",
-    "lib/viewmodels/profile_management/profile_management_viewmodel.dart",
-    "lib/viewmodels/ratings_reviews/ratings_reviews_viewmodel.dart",
+    "lib/viewmodels/dashboard/dashboard_viewmodel.dart",
+    "lib/viewmodels/service_management/service_management_viewmodel.dart",
+    "lib/viewmodels/booking_management/booking_management_viewmodel.dart",
+    "lib/viewmodels/customer_management/customer_management_viewmodel.dart",
+    "lib/viewmodels/reports_analytics/reports_analytics_viewmodel.dart",
+    "lib/viewmodels/notifications/notifications_viewmodel.dart",
     "lib/services/auth_service.dart",
+    "lib/services/service_service.dart",
     "lib/services/booking_service.dart",
-    "lib/services/payment_service.dart",
-    "lib/services/review_service.dart",
+    "lib/services/customer_service.dart",
+    "lib/services/report_service.dart",
+    "lib/services/notification_service.dart",
     "lib/utils/constants.dart",
     "lib/utils/helpers.dart",
 ]
 
-create_paths(folders_and_files)
+# create_paths(folders_and_files)
+
+
+def create_branches(branches):
+    for branch in branches:
+        try:
+            os.system(f"git checkout -b {branch}")
+            print("Branch created: ", branch)
+            os.system(f"git push origin {branch}")
+            print("branch pushed to git")
+
+        except OSError as e:
+            print(f"Error creating '{branch}': {e}")
+
+
+branches = branches = [
+    "feature/login-signup",
+    "feature/dashboard",
+    "feature/service-management",
+    "feature/booking-management",
+    "feature/customer-management",
+    "feature/reports-analytics",
+    "feature/notifications",
+]
+
+create_branches(branches)
